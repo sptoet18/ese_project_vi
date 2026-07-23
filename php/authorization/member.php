a9fb68459c732f8509d18aca2102e652101c2096
click18 <sean.toet@gmail.com>
2026-07-23 18:24:54 -0400

    Updated the consoles to print from the database

--- php/authorization/member.php
+++ php/authorization/member.php
@@ -1,86 +1,52 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+
     require_once __DIR__ . '/../util.php';
 
     session_start();
 
-    $elevatorPosition = getPositionImage(1);
-
-    class CanTransaction {
-        public string $sender;
-        public string $time;
-        public string $data;
-        public string $floor;
-        public string $last_floor;
-
-        public function __construct(
-            string $sender,
-            string $time,
-            string $data,
-            string $floor,
-            string $last_floor
-        ) {
-            $this->sender = $sender;
-            $this->time = $time;
-            $this->data = $data;
-            $this->floor = $floor;
-            $this->last_floor = $last_floor;
-        }
-    }
-
-    class ElevatorPosition {
-        public string $floor;
-        public string $last_floor;
-        public bool $is_moving;
-        public bool $is_closed;
-        public string $time;
-
-        public function __construct(
-            string $floor,
-            string $last_floor,
-            bool $is_moving,
-            bool $is_closed,
-            string $time,
-        ) {
-            $this->floor = $floor;
-            $this->last_floor = $last_floor;
-            $this->is_moving = $is_moving;
-            $this->is_closed = $is_closed;
-            $this->time = $time;
-        }
-    }
-
-    $transactions = [];
-    $transactions[] = new CanTransaction("0x200", "10:40:300", "0x05", "1", "3");
-    $transactions[] = new CanTransaction("0x203", "10:43:023", "0x01", "3", "1");
-
-    $positions = [];
-    $positions[] = new ElevatorPosition("1", "3", true, true, "10:40:000");
-
-    // $db = dbConnect('mysql:host=127.0.0.1; dbname=elevator', 'Emiliano', 'ESE');
+    $db = dbConnect('mysql:host=127.0.0.1; dbname=elevator', 'Emiliano', 'ESE');
     
-    // if (isset($_SESSION['username'])) {
+    if (isset($_SESSION['username'])) {
 
-    //     $username = $_SESSION['username'];
+        $username = $_SESSION['username'];
 
-    //     // database query for username
-    //     $userQuery = $db->prepare('
-    //         select id, username, hashed_password
-    //         from user
-    //         where username = :username
-    //     ');
-    //     $userQuery->execute(['username' => $username]);
-    //     $user = $userQuery->fetch();
+        // database query for username
+        $userQuery = $db->prepare('
+            select id, username, hashed_password
+            from user
+            where username = :username
+        ');
+        $userQuery->execute(['username' => $username]);
+        $user = $userQuery->fetch();
 
-    //     if ($user) {
-    //         // Add query to get the elevator's current location
+        if ($user) {
 
-    //         $elevatorPosition = getPositionImage(1);
-    //     } else {
-    //         echo "<script>location.href = \"/html/authorization/login.html\"</script>";
-    //     }
-    // } else {
-    //     echo "<script>location.href = \"/html/authorization/login.html\"</script>";
-    // }
+            $transactionQuery = $db->prepare('
+                select id, sent_by, transceived_at, data, message, current_floor, last_floor
+                from can_transaction
+                order by id desc
+            ');
+            $transactionQuery->execute([]);
+            $transactions = $transactionQuery->fetchAll(PDO::FETCH_ASSOC);
+            
+            $positionQuery = $db->prepare('
+                select id, current_floor, last_floor, is_moving, is_closed, recorded_at
+                from elevator_position
+                order by id desc
+            ');
+            $positionQuery->execute([]);
+            $positions = $positionQuery->fetchAll(PDO::FETCH_ASSOC);
+
+            $elevatorPosition = getPositionImage(1);
+        } else {
+            echo "<script>location.href = \"/html/authorization/login.html\"</script>";
+        }
+    } else {
+        echo "<script>location.href = \"/html/authorization/login.html\"</script>";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -112,7 +78,7 @@
 
             <section class="body">
                 <article>
-                    <div id="see"></div>"
+                    <div id="see"></div>
                 </article>
 
                 <article class="elevator-ui">
@@ -178,11 +144,11 @@
                                             <?php if (count($transactions)): ?>
                                                 <?php foreach ($transactions as $row): ?>
                                                     <tr>
-                                                        <td><?= htmlspecialchars($row->sender) ?></td>
-                                                        <td><?= htmlspecialchars($row->time) ?></td>
-                                                        <td><?= htmlspecialchars($row->data) ?></td>
-                                                        <td><?= htmlspecialchars($row->floor) ?></td>
-                                                        <td><?= htmlspecialchars($row->last_floor) ?></td>
+                                                        <td><?= htmlspecialchars($row['sent_by']) ?></td>
+                                                        <td><?= htmlspecialchars($row['transceived_at']) ?></td>
+                                                        <td><?= htmlspecialchars($row['data']) ?></td>
+                                                        <td><?= htmlspecialchars($row['current_floor']) ?></td>
+                                                        <td><?= htmlspecialchars($row['last_floor']) ?></td>
                                                     </tr>
                                                 <?php endforeach; ?>
                                             <?php else: ?>
@@ -209,14 +175,14 @@
                                         </thead>
 
                                         <tbody>
-                                            <?php if (count($positions)): ?>
+                                            <?php if (count($positions) > 0): ?>
                                                 <?php foreach ($positions as $row): ?>
                                                     <tr>
-                                                        <td><?= htmlspecialchars($row->floor) ?></td>
-                                                        <td><?= htmlspecialchars($row->time) ?></td>
-                                                        <td><?= htmlspecialchars($row->last_floor) ?></td>
-                                                        <td><?= htmlspecialchars($row->is_moving) ?></td>
-                                                        <td><?= htmlspecialchars($row->is_closed) ?></td>
+                                                        <td><?= htmlspecialchars($row['current_floor']) ?></td>
+                                                        <td><?= htmlspecialchars($row['recorded_at']) ?></td>
+                                                        <td><?= htmlspecialchars($row['last_floor']) ?></td>
+                                                        <td><?= htmlspecialchars($row['is_moving']) ?></td>
+                                                        <td><?= htmlspecialchars($row['is_closed']) ?></td>
                                                     </tr>
                                                 <?php endforeach; ?>
                                             <?php else: ?>
@@ -245,9 +211,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <script src="/js/components/top-bar.js"></script>
     <script src="/js/components/copyright.js" defer></script>
-    <script>
-        document.getElementById("date").textContent = new Date().toLocaleDateString();
-        document.getElementById("time").textContent = new Date().toLocaleTimeString();
-    </script>
 </body>
 </html>
